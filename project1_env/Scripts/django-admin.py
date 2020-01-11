475c82b55887578bd7cef3ad5f0c8e4d0028365c
John Dodd <johndodd90@gmail.com>
2020-01-11 22:16:19 +0000

    reconfiguring root folder

---
+++ project1_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\john\desktop\python specialist path\django\nick walter\halloffamevidsdeploy\project1_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
