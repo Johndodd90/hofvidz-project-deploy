da32abdaf66a26a015808cf845be4c65d677a19a
John Dodd <johndodd90@gmail.com>
2020-01-11 00:08:25 +0000

    deployment ready

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\john\desktop\python specialist path\django\nick walter\halloffamevids\hofvidz-project\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
